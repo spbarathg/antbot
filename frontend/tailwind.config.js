4068a0e53aed682733178f34f50ce4ef00554d73
Barath Ganesh <barathganesh64@gmail.com>
2025-03-29 12:04:19 +0530

    Remove node_modules from Git tracking and update .gitignore

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html'
   ],
+  safelist: [],
   theme: {
     extend: {
       fontFamily: {
